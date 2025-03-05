4f0388482dfa46e583e6c19485ccc236d2868c58
mlatinov <latinovmetodi@gmail.com>
2025-03-05 23:14:24 +0200

    add  stochastic gradient boosting

--- Models/GBMs/GBMs_Models/GBM_models.R
+++ Models/GBMs/GBMs_Models/GBM_models.R
@@ -3,7 +3,6 @@
 #### Libraries ####
 library(tidyverse)
 library(h2o)
-library(xgboost)
 library(gbm)
 
 ##### GBMs ####
@@ -128,15 +127,76 @@ tree_sp_grid$cv_error <- pmap_dbl(  # pmap_dbl() runs model_fit() on each row of
     n.minobsinnode = ..4)
   )
 
-## Safe the best params
+## Save the best params
 gbm_tune_params <- tree_sp_grid %>% arrange(cv_error) %>% head(1)
 
+## Stochastic GBM  with h2o ##
+
+# Start h2o
+Sys.setenv(JAVA_HOME = "C:/Program Files/Eclipse Adoptium/jdk-11.0.26.4-hotspot")
+h2o.init(max_mem_size = "4G")
+
+## Convert the training data into h2o object and assign higher weights to the minority class
+
+train_h2o_prep <- train_set
+train_h2o_prep$weights <- ifelse(train_set$churn == 1,1.88,0.68)
+  
+train_h2o <- as.h2o(train_h2o_prep)
+
+# Set the Y response and X predictors
+Y <- "churn"
+X <- setdiff(colnames(train_h2o),"churn")
+
+## Define a hyper grid 
+hyper_grid_h2o <- list(
+  sample_rate = c(0.5,0.75,1),
+  col_sample_rate = c(0.5,0.75,1),
+  col_sample_rate_per_tree = c(0.5,0.75,1)
+  )
+
+## Define a search strategy
+search_strategy <- list(
+  strategy = "RandomDiscrete", # Random Grid Search
+  stopping_metric = "auc",
+  stopping_tolerance = 0.001,
+  max_runtime_secs = 60 * 30,
+  stopping_rounds = 10
+  )
+
+## Perform the grid search 
+h2o_grid_gbm <- h2o.grid(
+  algorithm = "gbm",
+  grid_id = "grid_gbm",
+  x = X,
+  y = Y,
+  weights_column = "weights", # Assign higher weights to the minority class
+  training_frame = train_h2o,
+  hyper_params = hyper_grid_h2o,
+  search_criteria = search_strategy,
+  
+  # Take the best params from gbm_tune
+  ntrees = 2000,
+  learn_rate = gbm_tune_params$shrinkage,
+  max_depth = gbm_tune_params$interaction.depth,
+  min_rows = gbm_tune_params$n.minobsinnode,
+  
+  # Stopping Criteria
+  nfolds = 5,
+  stopping_rounds = 10,
+  stopping_tolerance = 0
+  )
+
+## Extract the results
+h2o_grid_perf <- h2o.getGrid(grid_id = "grid_gbm")
+
+# Take the best model id
+best_model_id <- h2o_grid_perf@model_ids[[1]]
+best_model <- h2o.getModel(best_model_id)
+
+## Save the models
 
 
-
-
-
-
+h2o.removeAll()
 
 
 
